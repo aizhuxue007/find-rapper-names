0d64dabfbdbf6da127501aba33c21bfc99b825fe
Aizhu Xue <aizhudeveloper@gmail.com>
2025-06-16 17:31:05 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const PORT = 8000;
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.listen(PORT, () => {
+  console.log(`server is running ${PORT}`);
+});
+
