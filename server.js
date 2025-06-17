391e51984ce90765189b62f83b4dc6a30064a42c
Aizhu Xue <aizhudeveloper@gmail.com>
2025-06-17 07:53:15 -0400

    add rappers obj as hard code data

--- server.js
+++ server.js
@@ -2,9 +2,34 @@ const express = require('express');
 const app = express();
 const PORT = 8000;
 
+// 1) create rapper data
+const rappers = {
+  "21 savage": {
+    "birthName": "Shéyaa Bin Abraham-Joseph",
+    "birthPlace": "Plaistow, London, England",
+    "age": 32 
+  },
+  "chance the rapper": {
+    "birthName":"Chancelor Bennett",
+    "birthPlace": "Chicago, Illinois",
+    "age": 32
+  },
+  "dylan": {
+    "birthName": "dylan",
+    "birthPlace": "dylan",
+    "age": 29 
+  }
+}
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
+
+// 2) add /rappers route
+console.log(rappers);
+// 3) add /:specificRapper route
+// 4) use cors middleware
+// 5) upload to vercel serverless functions
 
 app.listen(PORT, () => {
   console.log(`server is running ${PORT}`);
