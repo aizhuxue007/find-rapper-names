cb4dad7262e30f0257b74eee68ff614d7aabf025
Aizhu Xue <aizhudeveloper@gmail.com>
2025-07-01 13:59:10 -0400

    delete comments

--- api/index.js
+++ api/index.js
@@ -1,47 +1,39 @@
-const express = require('express');
-const cors = require('cors');
-const serverless = require('serverless-http');
+const express = require("express");
+const cors = require("cors");
+const serverless = require("serverless-http");
 const app = express();
 const PORT = 8000;
 
-// 1) create rapper data
 const rappers = {
   "21 savage": {
-    "birthName": "Shéyaa Bin Abraham-Joseph",
-    "birthPlace": "Plaistow, London, England",
-    "age": 32 
+    birthName: "Shéyaa Bin Abraham-Joseph",
+    birthPlace: "Plaistow, London, England",
+    age: 32,
   },
   "chance the rapper": {
-    "birthName":"Chancelor Bennett",
-    "birthPlace": "Chicago, Illinois",
-    "age": 32
+    birthName: "Chancelor Bennett",
+    birthPlace: "Chicago, Illinois",
+    age: 32,
   },
-  "dylan": {
-    "birthName": "dylan",
-    "birthPlace": "dylan",
-    "age": 29 
-  }
-}
+  dylan: {
+    birthName: "dylan",
+    birthPlace: "dylan",
+    age: 29,
+  },
+};
 
 app.use(cors());
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
 });
 
-// 2) add /rappers route
-app.get('/rappers', (req, res) => {
+app.get("/rappers", (req, res) => {
   res.json(rappers);
 });
 
-// 3) add /:specificRapper route
-app.get('/:rapperName', (req, res) => {
-  res.json(rappers[req.params.rapperName] || rappers['dylan']);
+app.get("/:rapperName", (req, res) => {
+  res.json(rappers[req.params.rapperName] || rappers["dylan"]);
 });
 
-// 4) use cors middleware
-
-// 5) upload to vercel serverless functions
-
 module.exports = serverless(app);
-
